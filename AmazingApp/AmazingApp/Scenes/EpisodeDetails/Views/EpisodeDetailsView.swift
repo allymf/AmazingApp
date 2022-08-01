73e8f5c2b3024f82f6b6d73b2ca9a2adb96254a9
alysson.moreira <alysson.moreira@baritecnologia.com.br>
2022-08-01 19:49:21 -0300

    feat(episodeDetails): Add scrollView to EpisodeDetailsView

--- AmazingApp/AmazingApp/Scenes/EpisodeDetails/Views/EpisodeDetailsView.swift
+++ AmazingApp/AmazingApp/Scenes/EpisodeDetails/Views/EpisodeDetailsView.swift
@@ -20,5 +20,60 @@ protocol EpisodeDetailsViewProtocol: ViewInitializer {
 }
 
 final class EpisodeDetailsView: UIView, EpisodeDetailsViewProtocol {
+    
+    // MARK: - Components
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        
+        scrollView.contentInset = .zero
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scrollView
+    }()
+
+    
+    private let contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     var viewModel: EpisodeDetailsViewModel?
+    
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        
+        addSubviews()
+        constrainSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubviews() {
+        addSubview(scrollView)
+        scrollView.addSubview(contentView)
+    }
+    
+    func constrainSubviews() {
+        NSLayoutConstraint.activate(
+            [
+                scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+                scrollView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+                scrollView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+                scrollView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+            ]
+        )
+        
+        NSLayoutConstraint.activate(
+            [
+                contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+                contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+                contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+                contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor)
+            ]
+        )
+    }
+    
 }
