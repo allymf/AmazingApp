48d35157fc57f7431306ad50606149281ede8fe2
alysson.moreira <alysson.moreira@baritecnologia.com.br>
2022-08-01 22:45:11 -0300

    feat(visualResources): Add helper method to set style in NavigationController

--- AmazingApp/AmazingApp/Extensions/UINavigationController+SetupStyle.swift
+++ AmazingApp/AmazingApp/Extensions/UINavigationController+SetupStyle.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import UIKit
+
+extension UINavigationController {
+    func setupStyle() {
+        let accentColor = UIColor(named: "AccentColor") ?? UIColor()
+        let mainColor = UIColor(named: "MainColor") ?? UIColor()
+        
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.backgroundColor = mainColor
+            appearance.titleTextAttributes = [.foregroundColor: accentColor]
+            appearance.largeTitleTextAttributes = [.foregroundColor: accentColor]
+
+            UINavigationBar.appearance().tintColor = accentColor
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().compactAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        } else {
+            UINavigationBar.appearance().tintColor = accentColor
+            UINavigationBar.appearance().barTintColor = mainColor
+            UINavigationBar.appearance().isTranslucent = false
+        }
+        
+    }
+    
+}
